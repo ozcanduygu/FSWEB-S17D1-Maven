b3085ea77f79e342b51df247a52e67f4e2b9b98e
ozcanduygu <duyguuozcann@hotmail.com>
2026-02-02 04:03:12 +0300

    Spring Boot Introduction

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -1,45 +1,39 @@
 package com.workintech.fswebs17d1.controller;
 
-// Bu kısımlar "yardımcı aletlerin" adresleri, kırmızıları bunlar bitirir:
+
 import com.workintech.fswebs17d1.entity.Animal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
-@RestController // Bu sınıf artık bir garson (API)
+@RestController
 @RequestMapping("/workintech/animal") // Giriş kapısı adresi
 public class AnimalController {
 
-    // Verilerin tutulduğu defter
     private Map<Integer, Animal> animals = new HashMap<>();
 
-    // application.properties dosyasından gelen bilgiler
     @Value("${course.name}")
     private String courseName;
 
     @Value("${project.developer.fullname}")
     private String developerName;
 
-    // [GET] Hepsini Listele
     @GetMapping
     public List<Animal> get() {
         return new ArrayList<>(animals.values());
     }
 
-    // [GET] Tek bir tanesini getir
     @GetMapping("/{id}")
     public Animal getById(@PathVariable Integer id) {
         return animals.get(id);
     }
 
-    // [POST] Yeni hayvan ekle
     @PostMapping
     public Animal save(@RequestBody Animal animal) {
         animals.put(animal.getId(), animal);
         return animal;
     }
 
-    // [PUT] Güncelleme
     @PutMapping("/{id}")
     public Animal update(@PathVariable Integer id, @RequestBody Animal animal) {
         // Eski hayvanı silip yenisini o ID ile ekliyoruz
@@ -48,7 +42,6 @@ public class AnimalController {
         return animal;
     }
 
-    // [DELETE] Silme
     @DeleteMapping("/{id}")
     public Animal delete(@PathVariable Integer id) {
         return animals.remove(id);
--- src/main/java/com/workintech/fswebs17d1/entity/Animal.java
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -1,21 +1,13 @@
 package com.workintech.fswebs17d1.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Animal {
     private Integer id;
     private String name;
-
-    public Animal(){
-
-    }
-    public Animal(Integer id, String name){
-        this.id= id;
-        this.name= name;
-    }
-    public Integer getId(){return id;}
-    public String getName(){return name;}
-
-    public void setId(Integer id){this.id=id;}
-    public void setName(String name){this.name=name;}
-
-
 }
